381f52ccff43bf0be7dba7ece85f81cc121e3728
Moosadeveloper <janmoosa77@gmail.com>
2024-06-07 19:55:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var name = prompt("what is your name");
+a="moosa"
+if(name=="mosa"){
+    alert("you are so close");
+}else if(name==a){
+    alert("wellcome");
+}else if(name=="musa"){
+    alert("you are so close");
+}else{
+    alert("Try again");
+}
+        
+    
